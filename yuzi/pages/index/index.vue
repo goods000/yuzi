6e9eac3d2d8e28bfb10fee50b9c196c467c8b8d4
yan大大 <2675351916@qq.com>
2023-12-26 15:20:14 +0800

    修改微信使用外部浏览器打开

--- yuzi/pages/index/index.vue
+++ yuzi/pages/index/index.vue
@@ -1,57 +1,65 @@
 <template>
 	<view class="content">
-		
-		<view class="main">
-			<view class="">
-				<!-- 轮播图 -->
-				<uni-swiper-dot :info="info" :current="current" mode="false">
-					<view class="swiperBox">
-						<view class="swiperBox-wrapper">
-							<swiper :indicator-dots="false" :autoplay="true" :interval="2500" :duration="1000" :circular="true" class="swiperBox-swiper" @change="changeSwiper">
-								<swiper-item class="swiperBox-swiper__item" v-for="(item, index) in info" :key="index">
-									<image :src="item" mode="widthFix" ></image>
-								</swiper-item>
-							</swiper>
-						</view> 
-					</view>
-				</uni-swiper-dot>
+		<view class="upIcon-box" v-if="isWeixin">
+			<view class="upIcon-wrapper">
+				<image class="img" src="@/static/up.png" mode=""></image>
+				<view class="upIcon-title">请使用外部浏览器打开</view>
 			</view>
 		</view>
 		
-		<view class="middel">
-			<view class="middel-top">
-				<view class="middel-item" @tap.stop="call('18589041101')">
-					<image src="@/static/service.png" mode=""></image>
-					<view class="itemBox">
-						<view class="title">24小时服务热线</view>
-						<view class="tips">185xxxx1101</view>
-					</view>
+		<view class="">
+			<view class="main">
+				<view class="">
+					<!-- 轮播图 -->
+					<uni-swiper-dot :info="info" :current="current" mode="false">
+						<view class="swiperBox">
+							<view class="swiperBox-wrapper">
+								<swiper :indicator-dots="false" :autoplay="true" :interval="2500" :duration="1000" :circular="true" class="swiperBox-swiper" @change="changeSwiper">
+									<swiper-item class="swiperBox-swiper__item" v-for="(item, index) in info" :key="index">
+										<image :src="item" mode="widthFix" ></image>
+									</swiper-item>
+								</swiper>
+							</view> 
+						</view>
+					</uni-swiper-dot>
 				</view>
-				<view class="middel-item" @tap.stop="$tools.jump('./code')">
-					<image src="@/static/wechat.png" mode=""></image>
-					<view class="itemBox">
-						<view class="title">官方微信</view>
-						<view class="tips">识别二维码，关注官方微信</view>
+			</view>
+			
+			<view class="middel">
+				<view class="middel-top">
+					<view class="middel-item" @tap.stop="call('18589041101')">
+						<image src="@/static/service.png" mode=""></image>
+						<view class="itemBox">
+							<view class="title">24小时服务热线</view>
+							<view class="tips">185xxxx1101</view>
+						</view>
 					</view>
-				</view>
-				<view class="middel-item" @tap.stop="getUrl()">
-					<image src="@/static/address.png" mode=""></image>
-					<view class="itemBox">
-						<view class="title">服务网点</view>
-						<view class="tips">看看距离服务网点有多远</view>
+					<view class="middel-item" @tap.stop="$tools.jump('./code')">
+						<image src="@/static/wechat.png" mode=""></image>
+						<view class="itemBox">
+							<view class="title">官方微信</view>
+							<view class="tips">识别二维码，关注官方微信</view>
+						</view>
+					</view>
+					<view class="middel-item" @tap.stop="getUrl()">
+						<image src="@/static/address.png" mode=""></image>
+						<view class="itemBox">
+							<view class="title">服务网点</view>
+							<view class="tips">看看距离服务网点有多远</view>
+						</view>
 					</view>
 				</view>
 			</view>
-		</view>
-		
-		
-		<view class="bottom">
-			<view class="left">
-				联系电话：185xxxx1101
-			</view>
-			<view class="right" @tap.stop="call('18589041101')">
-				<image src="@/static/phone.png" mode=""></image>
-				联系我们
+			
+			
+			<view class="bottom">
+				<view class="left">
+					联系电话：185xxxx1101
+				</view>
+				<view class="right" @tap.stop="call('18589041101')">
+					<image src="@/static/phone.png" mode=""></image>
+					联系我们
+				</view>
 			</view>
 		</view>
 		
@@ -62,6 +70,7 @@
 	export default {
 		data() {
 			return {
+				isWeixin: true,
 				info: [
 					'../../static/1.png','../../static/2.png','../../static/3.png'
 				],
@@ -73,9 +82,18 @@
 		onLoad() {
 		},
 		onShow() {
-			this.getWei();
+			this.init();
 		},
 		methods: {
+			init(){
+				this.isWx();
+				this.getWei();
+			},
+			isWx(){
+				var ua = navigator.userAgent.toLowerCase();
+				this.isWeixin = ua.indexOf('micromessenger') != -1;
+				console.log("isWeixin:",this.isWeixin);
+			},
 			// 导航
 			getUrl() {
 				let that = this;
@@ -236,5 +254,36 @@
 			}
 		}
 	}
+	
+	.upIcon-box{
+		position: absolute;
+		right: 5px;
+		top: 0;
+		right: 0;
+		z-index: 99;
+		width: 100%;
+		height: 100vh;
+		background: rgba(0, 0, 0, 0.5);
+	}
+	.upIcon-wrapper{
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		justify-content: flex-end;
+	}
+	.upIcon-wrapper .img{
+		width: 40px;
+		height: 40px;
+		text-align: right;
+		margin-right: 10px;
+		margin-bottom: 5px;
+		transform: rotate(90deg);
+	}
+	.upIcon-title{
+		font-size: 1rem;
+		color: #fff;
+		padding-right: 10px;
+	}
+	
 	
 </style>
